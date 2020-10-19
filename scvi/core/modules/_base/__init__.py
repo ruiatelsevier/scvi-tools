d9a9f1ba974ce5c457aba54632cffe840e2ed101
adamgayoso <adamgayoso@users.noreply.github.com>
2020-10-18 22:20:01 -0700

    sigmoid for qzv

--- scvi/core/modules/_base/__init__.py
+++ scvi/core/modules/_base/__init__.py
@@ -902,7 +902,7 @@ class EncoderTOTALVI(nn.Module):
             use_batch_norm=use_batch_norm,
         )
         self.z_mean_encoder = nn.Linear(n_hidden, n_output)
-        self.z_var_encoder = nn.Sequential(nn.Linear(n_hidden, n_output), nn.ReLU6())
+        self.z_var_encoder = nn.Sequential(nn.Linear(n_hidden, n_output), nn.Sigmoid())
 
         self.l_gene_encoder = FCLayers(
             n_in=n_input,
@@ -958,8 +958,8 @@ class EncoderTOTALVI(nn.Module):
         """
         # Parameters for latent distribution
         q = self.encoder(data, *cat_list)
-        qz_m = 7 * torch.tanh(0.1 * self.z_mean_encoder(q))
-        qz_v = self.z_var_encoder(q) + 1e-4
+        qz_m = 4 * torch.tanh(0.15 * self.z_mean_encoder(q))
+        qz_v = 3 * self.z_var_encoder(q) + 1e-4
         z, untran_z = self.reparameterize_transformation(qz_m, qz_v)
 
         ql_gene = self.l_gene_encoder(data, *cat_list)
